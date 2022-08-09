44440a6b4865a8192c734769e151954ca51a4dcd
Aleph235 <akram239911@gmail.com>
2022-08-09 16:49:50 +0200

    typo correction

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- creates the MySQL server user user_0d_1
 -- user_0d_1 should have all privileges on your MySQL server
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_db';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
