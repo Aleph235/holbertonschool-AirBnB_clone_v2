96aac06f35f3f69f283d32f1e0986d240c331182
Jebeniani <aymenjebeniani05@gmail.com>
2022-08-10 23:13:12 +0100

    city updates

--- models/city.py
+++ models/city.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Table, Column, Integer, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from sqlalchemy import Column, String, ForeignKey
 
 
 class City(BaseModel):
@@ -12,4 +9,4 @@ class City(BaseModel):
     __tablename__ = 'cities'
 
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
+    state_id = Column(String(60), nullable=False, ForeignKey("states.id"))
