7158ed6352bdeb43ebe2e68a5015113f4bf383a3
Aleph235 <akram239911@gmail.com>
2022-09-10 14:49:26 +0200

    typo correction

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -4,15 +4,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
     """hello hbnb return string"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def HBNB():
     """hbnb return string"""
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def Display_C(text):
@@ -28,10 +31,12 @@ def Display_Python(text="is cool"):
     text = text.replace('_', ' ')
     return "Python {}".format(text)
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def Display_Int(n):
     """If not int don't distplay"""
-    return "{} in a number".format(n)
+    return "{} is a number".format(n)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
