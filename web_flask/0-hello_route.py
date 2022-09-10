762dca19e6a91c5e8d0a801b918c45a6af1cddc9
Aleph235 <akram239911@gmail.com>
2022-09-10 13:52:00 +0200

    correcting requirements

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,17 @@
-#!/usr/bin env/python3
+#!/usr/bin/python3
 """flask simple application"""
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
     """hello hbnb return string"""
     return "Hello HBNB!"
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
+
     app.run(host="0.0.0.0")
-    
